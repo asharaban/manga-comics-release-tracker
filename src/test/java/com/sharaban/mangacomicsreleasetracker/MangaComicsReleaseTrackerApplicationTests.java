02fe401df13d13ceb732a9985001354c1f1cec6d
Alexander Sharaban <a.sharaban@aimprosoft.com>
2026-06-13 02:11:48 +0200

    initial commit

---
+++ src/test/java/com/sharaban/mangacomicsreleasetracker/MangaComicsReleaseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharaban.mangacomicsreleasetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaComicsReleaseTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
